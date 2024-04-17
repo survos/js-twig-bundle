7b91fe8af4764eaedc9b6b7420def30d17cea2e0
tacman <info@survos-ci.com>
2024-04-17 16:08:14 +0000

    get package names right, add controllers, etc.

---
+++ src/Components/DexieTwigComponent.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Survos\JsTwigBundle\Components;
+
+use Psr\Log\LoggerInterface;
+use Survos\JsTwigBundle\TwigBlocksTrait;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use Twig\Environment;
+
+#[AsTwigComponent(name: 'dexie', template: '@SurvosJsTwig/components/dexie_twig.html.twig')]
+final class DexieTwigComponent extends AsTwigComponent
+{
+    use TwigBlocksTrait;
+    public string $dbName; // required
+    public string $store; // required
+    public ?string $refreshEvent=null;
+
+    public function __construct(
+        private Environment $twig,
+        private LoggerInterface $logger,
+        private array $config=[]
+    ) {
+
+        //        ='@survos/grid-bundle/api_grid';
+    }
+
+
+    public function getRefreshEvent(): string
+    {
+        return $this->refreshEvent;
+    }
+
+    public function getFilter()
+    {
+        return $this->filter;
+    }
+
+    public function getStore(): string
+    {
+        return $this->store;
+    }
+    public $filter = null;
+    public array $order = [];
+
+
+    public function getTwigTemplate(): string
+    {
+        return $this->getTwigBlocks()['twig_template'];
+    }
+
+    public function getSchema(): array
+    {
+        #    db.version(3).stores({
+#savedTable: "id,name,owned",
+#productTable: "++id,price,brand,category"
+#});
+
+        $schema = [];
+        foreach ($this->config['stores'] as $store) {
+            $schema[$store['name']] = $store['schema'];
+        }
+        return $schema;
+    }
+
+    public function getDbName()
+    {
+        return $this->config['db'];
+    }
+    public function getVersion(): int
+    {
+        return $this->config['version'];
+    }
+
+
+
+}
--- src/SurvosJsTwigBundle.php
+++ src/SurvosJsTwigBundle.php
@@ -2,14 +2,20 @@
 
 namespace Survos\JsTwigBundle;
 
+use Survos\CoreBundle\Traits\HasAssetMapperTrait;
+use Survos\JsTwigBundle\Components\DexieTwigComponent;
 use Survos\JsTwigBundle\Components\JsTwigComponent;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class SurvosJsTwigBundle extends AbstractBundle
 {
+    use HasAssetMapperTrait;
+
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
 
@@ -17,8 +23,14 @@ class SurvosJsTwigBundle extends AbstractBundle
             ->setAutowired(true)
             ->setAutoconfigured(true)
             ->setArgument('$twig', new Reference('twig'))
-            ->setArgument('$logger', new Reference('logger', ContainerInterface::NULL_ON_INVALID_REFERENCE))
-        ;
+            ->setArgument('$logger', new Reference('logger', ContainerInterface::NULL_ON_INVALID_REFERENCE));
+
+        $builder->register(DexieTwigComponent::class)
+            ->setAutowired(true)
+            ->setAutoconfigured(true)
+            ->setArgument('$config', $config)
+            ->setArgument('$twig', new Reference('twig'))
+            ->setArgument('$logger', new Reference('logger', ContainerInterface::NULL_ON_INVALID_REFERENCE));
 
     }
 
@@ -27,6 +39,28 @@ class SurvosJsTwigBundle extends AbstractBundle
         $definition->rootNode()
             ->children()
             ->booleanNode('debug')->defaultFalse()->end()
+            ->scalarNode('version')->defaultValue(1)->end()
+            ->scalarNode('db')->defaultValue('db')->end()
+            ->arrayNode('stores')
+            ->arrayPrototype()
+            ->children()
+            ->scalarNode('name')->info("the store name")->example("friendTable")->end()
+            ->scalarNode('schema')->info("the index definition")->example("++i,age")->end()
+            ->scalarNode('url')->info("the API to use to load if empty.  json-ld iterates through pages")
+            ->example("/api/friends")
+            ->end()
+            ->end()
+            ->end()
+            ->end()
             ->end();
     }
+
+
+    public function getPaths(): array
+    {
+        $dir = realpath(__DIR__ . '/../assets/');
+        assert(file_exists($dir), 'asset path must exist for the assets in ' . __DIR__);
+        return [$dir => '@survos/js-twig'];
+    }
+
 }
